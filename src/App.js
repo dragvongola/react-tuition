c51c680a2df9df27e17604a24d5dd8b2144e65da
azril <dragvongola@gmail.com>
2020-05-27 01:57:40 +0800

    first draft

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "styles.css";
+import "tailwindcss/dist/base.css";
+import AnimationRevealPage from "helpers/AnimationRevealPage";
+import Hero from "components/hero/TwoColumnWithInput";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AnimationRevealPage>
+      <Hero />
+    </AnimationRevealPage>
   );
 }
 
 export default App;
+
+//src\components\hero\TwoColumnWithInput.js
